84a7c60504f424fd98ec53361dc299d63f7bbf30
Kiran Smelser <kiran.smelser@gmail.com>
2022-05-16 09:02:47 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Copies an entire folder and its contents into # a ZIP file whose filename
+increments.
+"""
+
+import zipfile, os
+
+
+def backup_to_zip(folder):
+
+
+def main():
+    folder_name = input("Please enter the name of the folder you would like to backup: ")
+    backup_to_zip(folder_name)
+
+
+if __name__ == "__main__":
+    main()
